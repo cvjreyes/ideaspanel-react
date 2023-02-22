bb9744207131c9f364071724819bd79dadf08b3c
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-02-22 12:17:26 +0100

    Nuevos componentes y post del login

---
+++ src/components/general/Button.jsx
@@ -0,0 +1,53 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+
+export default function Button({
+  name,
+  text,
+  onClick,
+  bgColor,
+  color,
+  border,
+  borderRadius,
+  width,
+  margin,
+  className,
+  fontWeight,
+  fontSize,
+  type,
+  padding,
+  bgHover,
+  display,
+  disabled,
+}) {
+  const buttonStyle = {
+    width: width || "100%",
+    padding: padding || "10px 20px",
+    background: disabled ? "lightgray" : bgColor,
+    color,
+    border,
+    borderColor: disabled && "lightgray",
+    borderRadius: borderRadius || "6px",
+    margin,
+    fontWeight,
+    fontSize,
+    ":hover": {
+      background: disabled ? "auto" : bgHover,
+    },
+    display: display,
+  };
+
+  return (
+    <button
+      type={type}
+      className={`${className} ${disabled ? "not-allowed" : "pointer"}`}
+      css={buttonStyle}
+      onClick={onClick}
+      name={name}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+}
---
+++ src/components/general/Input.jsx
@@ -0,0 +1,33 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
+
+export default function Input({
+  id,
+  name,
+  value,
+  type,
+  onChange,
+  className,
+  placeholder,
+}) {
+  return (
+    <input
+      css={InputStyle}
+      id={id}
+      name={name}
+      value={value}
+      type={type}
+      onChange={onChange}
+      className={className}
+      placeholder={placeholder}
+    />
+  );
+}
+
+const InputStyle = {
+  height: "40px",
+  border: "1px solid #D2D2D2",
+  borderRadius: "6px",
+  padding: "5px",
+};
