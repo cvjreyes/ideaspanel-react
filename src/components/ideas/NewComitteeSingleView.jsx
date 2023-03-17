70ead8b55e57fb7e105c1149d5c88d01331aeb4e
Toni <webdevtoni@gmail.com>
2023-03-17 13:04:35 +0100

    comittee working?

--- src/components/ideas/NewComitteeSingleView.jsx
+++ src/components/ideas/NewComitteeSingleView.jsx
@@ -16,27 +16,44 @@ import Loading from "../general/Loading";
 import thumbsUp from "../../assets/images/thumbs-up.png";
 import thumbsDown from "../../assets/images/thumbs-down.png";
 import Back from "../../assets/images/back.png";
+import { useNotifications } from "reapop";
 
 export default function NewComitteeSingleView() {
   const [_, params] = useRoute("/comittee/:idea_id");
   const { user } = useContext(AuthContext);
   const [__, navigate] = useLocation();
+  const { notify } = useNotifications();
 
   const [data, setData] = useState(null);
 
+  const getData = async () => {
+    const { body } = await api(
+      "get",
+      `/ideas/get_info_and_vote/${params.idea_id}`
+    );
+    setData(body);
+  };
+
   useEffect(() => {
     if (!user.isComittee) return navigate("/");
-    const getData = async () => {
-      const { body } = await api("get", `/ideas/get_info/${params.idea_id}`);
-      setData(body);
-    };
     getData();
   }, []);
 
+  const handleVote = async (vote) => {
+    const { ok } = await api("post", "/comittee_votes/submit_vote", {
+      idea_id: data.id,
+      user_id: user.id,
+      vote,
+    });
+    if (!ok) return notify("Something went wrong", "error");
+    notify("Vote successfully done", "success");
+    getData();
+  };
+
   if (!data) return <Loading />;
   return (
-    <div css={singleViewStyle} className="page_title">
-      <div className="top">
+    <div css={singleViewStyle}>
+      <div className="top page_title">
         <ButtonWithImage
           src={Back}
           onClick={() => navigate("/comittee")}
@@ -47,39 +64,56 @@ export default function NewComitteeSingleView() {
         <CountdownTimer date={data.sent_to_validate_at} />
       </div>
       <Card item={{ ...data, anonymous: true }} comittee={true} />
-      <BoxVotes />
+      <BoxVotes handleVote={handleVote} approved={data.approved} />
+      <p className="remove pointer" onClick={() => handleVote(null)}>
+        Remove vote
+      </p>
     </div>
   );
 }
 
-const BoxVotes = () => {
+const BoxVotes = ({ handleVote, approved }) => {
   return (
     <div className="boxVotes">
-      <ButtonWithImage
-        type="button"
-        bgColor={colors["red"].background}
-        bgHover={colors["red"].backgroundHover}
-        width="50px"
-        onClick={() => handleVote(0)}
-        // img
-        src={thumbsDown}
-      />
-      <ButtonWithImage
-        type="button"
-        bgColor={colors["green"].background}
-        bgHover={colors["green"].backgroundHover}
-        width="50px"
-        onClick={() => handleVote(1)}
-        // img
-        src={thumbsUp}
-      />
+      <VoteWrapper voted={approved !== null && !approved}>
+        <ButtonWithImage
+          type="button"
+          bgColor={colors["red"].background}
+          bgHover={colors["red"].backgroundHover}
+          width="50px"
+          onClick={() => handleVote(0)}
+          // img
+          src={thumbsDown}
+        />
+      </VoteWrapper>
+      <VoteWrapper voted={approved}>
+        <ButtonWithImage
+          type="button"
+          bgColor={colors["green"].background}
+          bgHover={colors["green"].backgroundHover}
+          width="50px"
+          onClick={() => handleVote(1)}
+          // img
+          src={thumbsUp}
+        />
+      </VoteWrapper>
     </div>
   );
 };
 
+const VoteWrapper = ({ children, voted }) => {
+  const voteWrapperStyle = {
+    padding: "5px",
+    border: voted && "2px solid blue",
+    borderRadius: "6px",
+  };
+
+  return <div css={voteWrapperStyle}>{children}</div>;
+};
+
 const singleViewStyle = {
   width: "fit-content",
-  margin: "100px auto",
+  margin: "100px auto 0",
   ".top": {
     display: "grid",
     gridTemplateColumns: "repeat(3, 1fr)",
@@ -92,5 +126,13 @@ const singleViewStyle = {
     margin: "0 auto",
     display: "flex",
     justifyContent: "space-evenly",
+  },
+  ".remove": {
+    textAlign: "center",
+    marginTop: "30px",
+    color: "blue",
+    ":hover": {
+      textDecoration: "underline",
+    },
   },
 };
