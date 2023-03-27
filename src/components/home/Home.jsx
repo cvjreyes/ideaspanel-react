9b4395f2fd75c2063605b6624c842212e948cbcf
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-03-27 08:36:24 +0200

    First pagination implemented

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,24 +11,57 @@ import NoResults from "./NoResults";
 
 export default function Home() {
   const [data, setData] = useState(null);
-
+  const [totalPages, setTotalPages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  
+  const getData = async () => {
+    const { body } = await api("get", `/ideas/get_some/${currentPage}`);
+    setData(body);
+  };
+  
   useEffect(() => {
-    const getData = async () => {
-      const { body } = await api("get", "/ideas/get_some/0");
-      setData(body);
+    const getPages = async () => {
+      const { body: pages } = await api("get", "/ideas/get_page_published");
+      const pagesArray = [];
+      for (let i = 0; i < pages; i++) {
+        pagesArray.push(i + 1);
+      }
+      setTotalPages(pagesArray);
     };
     getData();
+    getPages();
   }, []);
+
+  useEffect(() => {
+    getData();
+  }, [currentPage]);
+
+  function handleClickPage(e) {
+    setCurrentPage(e - 1);
+  }
 
   return (
     <div css={homeStyle}>
       <h1 className="page_title">Ideas Panel</h1>
       {data ? (
         data.length > 0 ? (
-          <div className="map">
-            {data.map((item, i) => {
-              return <Card item={item} key={i} />;
-            })}
+          <div>
+            <div className="pagination">
+              {totalPages?.map((page) => (
+                <div
+                  key={page}
+                  className={page === currentPage + 1 ? "active" : ""}
+                  onClick={() => handleClickPage(page)}
+                >
+                  {page}
+                </div>
+              ))}
+            </div>
+            <div className="map">
+              {data.map((item, i) => {
+                return <Card item={item} key={i} />;
+              })}
+            </div>
           </div>
         ) : (
           <NoResults />
@@ -49,5 +82,24 @@ const homeStyle = {
     gridTemplateColumns: "repeat(auto-fit, minmax(100px, 320px))",
     marginTop: "50px",
     gap: "50px",
+  },
+  ".pagination": {
+    display: "flex",
+    justifyContent: "start",
+    alignItems: "center",
+    margin: "20px 0 0 8%",
+    div: {
+      display: "inline-block",
+      margin: "0 5px",
+      padding: "5px 10px",
+      border: "1px solid #ccc",
+      borderRadius: "5px",
+      cursor: "pointer",
+    },
+    ".active": {
+      backgroundColor: "#0077cc",
+      color: "#fff",
+      border: "none",
+    },
   },
 };
