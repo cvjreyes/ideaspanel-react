031529a7aef5a36ccf026cf307b2a1f88dcb15fa
Nacho <nachogonzalez.v.99@gmail.com>
2023-04-06 10:17:02 +0200

    New style in home page and Full Section component

---
+++ src/components/general/FullSection.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+
+const FullSection = styled.div({
+    height: "100vh",
+    padding: "6rem 5vw 1rem 5vw",
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+})
+
+export { FullSection}
+
+
